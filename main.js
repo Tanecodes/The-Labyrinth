0f8e4d93c0e13bfe696fe89eba8aefeeb1a3eded
Tanecodes <tanecodes@gmail.com>
2025-06-28 00:45:26 +1000

    added some audio for the start button and main menu music and added a fade out transition when clicked pretty happy with the results

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const startBtn = document.getElementById("startBtn");
+const startMenu = document.getElementById("startMenu");
+
 const menuMusic = document.getElementById("menuMusic");
 
+const btnAudio = document.getElementById("btnAudio");
+btnAudio.volume = 0.07;
+
+const hoverAudio = document.getElementById("hoverAudio");
+hoverAudio.volume = 1;
+
 startBtn.addEventListener("click", function() {
+  btnAudio.play();
   menuMusic.play();
-  startBtn.style.display = "none";
+
+  startMenu.classList.add("fade-out");
+
+  setTimeout(function() {
+    startMenu.style.display = "none";
+  }, 2000);
 });
