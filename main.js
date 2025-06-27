e7209be58c2ea0c7c548ec2a58bfbc636280307b
Tanecodes <tanecodes@gmail.com>
2025-06-28 02:27:30 +1000

    changed menu music and created a dynamic div element to hold main menu buttons no styling yet its sleep time zzzzzz

--- main.js
+++ main.js
@@ -1,21 +1,61 @@
+// creating some main variables
 const startBtn = document.getElementById("startBtn");
 const startMenu = document.getElementById("startMenu");
 
+// menu music
 const menuMusic = document.getElementById("menuMusic");
+menuMusic.volume = 0.3;
 
+// button click audio
 const btnAudio = document.getElementById("btnAudio");
 btnAudio.volume = 0.07;
 
+// button hover audio (unfinished).
 const hoverAudio = document.getElementById("hoverAudio");
-hoverAudio.volume = 1;
 
+// start button event listener
 startBtn.addEventListener("click", function() {
   btnAudio.play();
-  menuMusic.play();
 
   startMenu.classList.add("fade-out");
 
   setTimeout(function() {
     startMenu.style.display = "none";
   }, 2000);
+
+  setTimeout(function() {
+    menuMusic.play();
+  }, 1500);
+
+  // creating dynamic main menu elements (div wrapper, buttons etc..)
+  const mainMenu = document.createElement("div");
+  mainMenu.classList.add("mainMenu");
+
+  document.body.appendChild(mainMenu);
+
+  const beginBtn = document.createElement("button");
+  const settingsBtn = document.createElement("button");
+  const loadBtn = document.createElement("button");
+  const creditsBtn = document.createElement("button");
+
+  // setting text content and adding classes to the buttons
+  
+  beginBtn.textContent = "Begin Adventure";
+  beginBtn.classList.add("menuBtn");
+
+  settingsBtn.textContent = "Settings";
+  settingsBtn.classList.add("menuBtn");
+
+  loadBtn.textContent = "Load";
+  loadBtn.classList.add("menuBtn");
+
+  creditsBtn.textContent = "Credits";
+  creditsBtn.classList.add("menuBtn");
+
+  // appending menu buttons to main menu div.
+  mainMenu.appendChild(beginBtn);
+  mainMenu.appendChild(settingsBtn);
+  mainMenu.appendChild(loadBtn);
+  mainMenu.appendChild(creditsBtn);
+
 });
