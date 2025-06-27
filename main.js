9ea7c5b73019cb4dcce681e41795b131440d29db
Tanecodes <tanecodes@gmail.com>
2025-06-28 07:30:13 +1000

    created main menu and added audio also all wrapped inside a setTimeout method and tried reorganizingthe code so it looks a little cleaner probably looks like shit though

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const startMenu = document.getElementById("startMenu");
 
 // menu music
 const menuMusic = document.getElementById("menuMusic");
-menuMusic.volume = 0.3;
+menuMusic.volume = 0.2;
 
 // button click audio
 const btnAudio = document.getElementById("btnAudio");
@@ -12,6 +12,7 @@ btnAudio.volume = 0.07;
 
 // button hover audio (unfinished).
 const hoverAudio = document.getElementById("hoverAudio");
+hoverAudio.volume = 0.5;
 
 // start button event listener
 startBtn.addEventListener("click", function() {
@@ -29,33 +30,49 @@ startBtn.addEventListener("click", function() {
 
   // creating dynamic main menu elements (div wrapper, buttons etc..)
   const mainMenu = document.createElement("div");
-  mainMenu.classList.add("mainMenu");
+  mainMenu.id = "mainMenu";
 
-  document.body.appendChild(mainMenu);
+  setTimeout(function() {
 
-  const beginBtn = document.createElement("button");
-  const settingsBtn = document.createElement("button");
-  const loadBtn = document.createElement("button");
-  const creditsBtn = document.createElement("button");
+      const beginBtn = document.createElement("button");
+      const settingsBtn = document.createElement("button");
+      const loadBtn = document.createElement("button");
+      const creditsBtn = document.createElement("button");
 
-  // setting text content and adding classes to the buttons
-  
-  beginBtn.textContent = "Begin Adventure";
-  beginBtn.classList.add("menuBtn");
+      // setting text content and adding classes to the buttons
+      beginBtn.textContent = "Begin Adventure";
+      beginBtn.classList.add("menuBtn");
 
-  settingsBtn.textContent = "Settings";
-  settingsBtn.classList.add("menuBtn");
+      settingsBtn.textContent = "Settings";
+      settingsBtn.classList.add("menuBtn");
 
-  loadBtn.textContent = "Load";
-  loadBtn.classList.add("menuBtn");
+      loadBtn.textContent = "Load";
+      loadBtn.classList.add("menuBtn");
 
-  creditsBtn.textContent = "Credits";
-  creditsBtn.classList.add("menuBtn");
+      creditsBtn.textContent = "Credits";
+      creditsBtn.classList.add("menuBtn");
 
-  // appending menu buttons to main menu div.
-  mainMenu.appendChild(beginBtn);
-  mainMenu.appendChild(settingsBtn);
-  mainMenu.appendChild(loadBtn);
-  mainMenu.appendChild(creditsBtn);
+      // appending menu buttons to main menu div.
+      mainMenu.appendChild(beginBtn);
+      mainMenu.appendChild(settingsBtn);
+      mainMenu.appendChild(loadBtn);
+      mainMenu.appendChild(creditsBtn);
 
+      document.body.appendChild(mainMenu);
+
+      const menuBtns = document.querySelectorAll(".menuBtn");
+      menuBtns.forEach(function(buttons) {
+        buttons.addEventListener("mouseenter", function() {
+          hoverAudio.play();
+          hoverAudio.currentTime = 0;
+        });
+
+        buttons.addEventListener("click", function() {
+          btnAudio.play();
+          btnAudio.currentTime = 0;
+        });
+      });
+    }, 2000);
 });
+
+
