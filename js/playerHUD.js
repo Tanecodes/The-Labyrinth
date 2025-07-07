1e9c1384aff237f076ee5295005961434f9583e5
Tanecodes <tanecodes@gmail.com>
2025-07-08 07:50:40 +1000

    creating menu modal for playerHUD for volume,options,load  etc.

--- js/playerHUD.js
+++ js/playerHUD.js
@@ -2,6 +2,10 @@ const HUDcontainer = document.getElementById("HUDcontainer");
 const playerPortrait = document.getElementById("playerPortrait");
 const playerBag = document.getElementById("playerBag");
 
+const playerMenuBtn = document.getElementById("playerMenuBtn");
+const playerMenuPanel = document.getElementById("playerMenuPanel");
+const playerContinueBtn = document.getElementById("playerContinueBtn");
+
 const playerPath = {
   male: "images/characters/male-portrait.png",
   female: "images/characters/female-01.png"
@@ -19,3 +23,10 @@ function displayHUD() {
   }, 3000);
 };
 
+playerMenuBtn.addEventListener("click", function() {
+  playerMenuPanel.style.display = "flex";
+});
+
+ playerContinueBtn.addEventListener("click", function() {
+    playerMenuPanel.style.display = "none";
+  });
