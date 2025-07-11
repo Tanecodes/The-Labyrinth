40044ecc8e31d508de1d8adb0319d49f938912fe
Tanecodes <tanecodes@gmail.com>
2025-07-12 00:05:33 +1000

    started coding up the setBattleBackground function and some overlay code to create a nice animation to switch into battle scenes but son is awake and its 12.05am so start fresh in the morning <3

--- js/fightlogic.js
+++ js/fightlogic.js
@@ -1,11 +1,27 @@
 // battle scene logic and battle mechanics
 // this function is gonna hold most if not all things to do with the battle mechanics
 
+function setBattleBackground(imagePath) {
+  sceneContainer.style.display = "block";
+  sceneContainer.style.position = "relative";
+
+  const battleOverlay = document.createElement("div");
+  battleOverlay.id = "battleOverlay";
+  battleOverlay.style.pointerEvents = "none";
+  document.body.appendChild(battleOverlay);
+
+  void battleOverlay.offsetWidth;
+
+  battleOverlay.style.opacity = "1";
+};
+
 function startFight () {
   // grabbing normal HUD and hiding it
   const normalHud = document.getElementById("HUDcontainer");
   normalHud.style.display = "none";
- 
+  
+  
+
 
 
   normalHud.style.display = "block";
